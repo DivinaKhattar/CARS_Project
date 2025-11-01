59de2bcb3fc17c1338e8843368d25d5312f2ad36
DivinaKhattar <divinakhattar@gmail.com>
2025-11-01 18:46:54 +0530

    Import Flask and related libraries in __init__.py
    Added necessary imports for Flask, SQLAlchemy, LoginManager, Mail, BackgroundScheduler, and dotenv.

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,11 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_mail import Mail
+from apscheduler.schedulers.background import BackgroundScheduler
+from dotenv import load_dotenv
+
 def create_app():
     app = Flask(__name__)
 
