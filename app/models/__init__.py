029cb726da730fb6f82c6b7bc9547c72a5e0c7fb
DivinaKhattar <divinakhattar@gmail.com>
2025-11-01 19:15:57 +0530

    Import SummaryOfferCostEntry in models init file

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,6 +4,7 @@ from .rsqr import RSQR
 from .management_council import ManagementCouncil
 from .offer_evaluation import OfferEvaluation
 from .summary_offer import SummaryOffer, MilestoneEntry
+from app.models.summary_offer_cost_entry import SummaryOfferCostEntry
 from .nda_soc import NDASOC
 from .uo_number import UONumber, UOCostEntry
 from .unique_sanction import UniqueSection
